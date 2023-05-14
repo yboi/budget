5310d34dedb411f2d45d8f36f2a95d172e35ac1d
yboi <yaroslav@enapps.co.uk>
2023-05-14 09:21:17 +0300

    [FIX] Refactoring

--- create_db.py
+++ create_db.py
@@ -1,16 +1,12 @@
 import os.path
-from models import Base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 
-
+Base = declarative_base()
 db_name = "main.db"
 engine = create_engine(f"sqlite:///{db_name}")
-Session = sessionmaker(bind=engine)
-session = Session()
 
 if os.path.isfile(db_name):
-    # engine.connect()
     print("DB is exists")
 else:
     Base.metadata.create_all(engine)
--- models.py
+++ models.py
@@ -1,3 +1,35 @@
 from sqlalchemy.orm import declarative_base
+from create_db import engine
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import sessionmaker
+
 
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+
+class CreateUser(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    login: Mapped[[str]] = mapped_column(String(30), unique=True)
+    user_name: Mapped[str] = mapped_column(String(30))
+    email: Mapped[str] = mapped_column(String(30))
+    password: Mapped[str] = mapped_column(String(30))
+
+    def __init__(self, login, user_name, email, password):
+        self.login = login
+        self.user_name = user_name
+        self.email = email
+        self.password = password
+
+        with Session() as session:
+            user = session.query(CreateUser).filter_by(login=self.login).first()
+            if user:
+                raise ValueError("Login already exists")
+            session.add(self)
+            session.commit()
+            print(f"User {self.user_name} with email {self.email} created successfully! (login:{self.login}  password:{self.password})")
